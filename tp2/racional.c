7c8858bf58b924fadb7cdb7522f8fa33c805d8df
Victoria Aviles <victoria.aviles.chavez@gmail.com>
2026-04-03 01:26:42 -0300

    terminei de fazer as funcoes do racional.c

--- tp2/racional.c
+++ tp2/racional.c
@@ -1,23 +1,6 @@
-/* 
- * Tipos Abstratos de Dados - TADs
- * Arquivo de implementação para TAD racional.
- * Feito em 20/09/2024 para a disciplina CI1001 - Programação 1.
- *
- * Este arquivo deve conter as implementações das funções cujos protótipos
- * foram definidos em racional.h. Neste arquivo também podem ser definidas
- * funções auxiliares para facilitar a implementação daquelas funções.
-*/
-
-/* coloque aqui seus includes (primeiro os <...>, depois os "...") */
 #include <stdio.h>
 #include <stdlib.h>
 #include "racional.h"
-
-/*
- * Implemente aqui as funcoes definidas no racionais.h; caso precise,
- * pode definir aqui funcoes auxiliares adicionais, que devem ser usadas
- * somente neste arquivo.
-*/
 
 /* retorna um número aleatório entre min e max, inclusive. */
 long aleat (long min, long max)
@@ -50,7 +33,8 @@ long mdc (long a, long b)
 /* mmc = (a * b) / mdc (a, b)        */
 long mmc (long a, long b)
 {
-  if (a == 0 || b == 0) return 0;
+  if (a == 0 || b == 0) 
+    return 0;
   return (a * b) / mdc (a, b);
 }
 
@@ -82,9 +66,9 @@ struct racional simplifica_r (struct racional r)
 
 /* Retorna o numerador do racional r */
 long numerador_r (struct racional r)
-  {
-    return r.num;
-  }
+{
+  return r.num;
+}
 
 /* Retorna o denominador do racional r */
 long denominador_r (struct racional r)
@@ -166,7 +150,7 @@ int compara_r (struct racional r1, struct racional r2)
 
   long up = r1.num * r2.den;
 
-  long down = r2.num * r1.num;
+  long down = r2.num * r1.den;
 
   if (up < down)
     return -1;
@@ -180,20 +164,60 @@ int compara_r (struct racional r1, struct racional r2)
 /* Retorna a soma dos racionais r1 e r2 no parametro *r3.
  * Retorna 1 se a operacao foi bem sucedida ou
  *         0 se r1 ou r2 for inválido ou se *r3 for nulo */
-int soma_r (struct racional r1, struct racional r2, struct racional *r3);
+int soma_r (struct racional r1, struct racional r2, struct racional *r3)
+{
+  if (!valido_r (r1) || !valido_r (r2) || r3 == NULL )
+    return 0;
+  r3 -> num = (r1.num * r2.den) + (r2.num * r1.den);
+  r3 -> den = r1.den * r2.den;
+
+  *r3 = simplifica_r (*r3);
+
+  return 1;
+}
 
 /* Retorna a subtracao dos racionais r1 e r2 no parametro *r3.
  * Retorna 1 se a operacao foi bem sucedida ou
  *         0 se r1 ou r2 for inválido ou se *r3 for nulo */
-int subtrai_r (struct racional r1, struct racional r2, struct racional *r3);
+int subtrai_r (struct racional r1, struct racional r2, struct racional *r3)
+{
+  if (!valido_r (r1) || !valido_r (r2) || r3 == NULL )
+    return 0;
+  r3 -> num = (r1.num * r2.den) - (r2.num * r1.den);
+  r3 -> den = r1.den * r2.den;
+
+  *r3 = simplifica_r (*r3);
+
+  return 1;
+}
 
 /* Retorna a multiplicacao dos racionais r1 e r2 no parametro *r3.
  * Retorna 1 se a operacao foi bem sucedida ou
  *         0 se r1 ou r2 for inválido ou se *r3 for nulo */
-int multiplica_r (struct racional r1, struct racional r2, struct racional *r3);
+int multiplica_r (struct racional r1, struct racional r2, struct racional *r3)
+{
+  if (!valido_r (r1) || !valido_r (r2) || r3 == NULL)
+    return 0;
+  r3 -> num = r1.num * r2.num;
+  r3 -> den = r1.den * r2.den;
+
+  *r3 = simplifica_r (*r3);
+
+  return 1;
+}
 
 /* Retorna a divisao dos racionais r1 e r2 no parametro *r3.
  * Retorna 1 se a operacao foi bem sucedida ou
  *         0 se r1 ou r2 for inválido ou se *r3 for nulo */
-int divide_r (struct racional r1, struct racional r2, struct racional *r3);
+int divide_r (struct racional r1, struct racional r2, struct racional *r3)
+{
+  if (!valido_r (r1) || !valido_r (r2) || r3 == NULL || r2.num == 0)
+    return 0;
+  r3 -> num = r1.num * r2.den;
+  r3 -> den = r1.den * r2.num;
+
+  *r3 = simplifica_r (*r3);
+
+  return 1;
+}
 
