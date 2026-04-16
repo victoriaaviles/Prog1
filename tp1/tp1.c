467915bf969b49223ad7ecb808733203289c5678
victoriaaviles <victoria.aviles.chavez@gmail.com>
2026-04-16 14:21:08 -0300

    "fiz algumas funcoes do racional.c"

---
+++ tp1/tp1.c
@@ -0,0 +1,65 @@
+/* 
+ * Tipos Abstratos de Dados - TADs
+ * Arquivo do programa principal, que usa o TAD racional.
+ * Feito em 16/09/2024 para a disciplina CI1001 - Programação 1.
+*/
+
+/* coloque aqui seus includes (primeiro os <...>, depois os "...") */
+#include <stdio.h>
+#include <stdlib.h>
+#include "racional.h"
+
+/* programa principal */
+int main ()
+{
+    srand (0); /* use assim, com zero */
+
+    long n, max;
+
+    scanf ("%ld", &n);
+    scanf ("%ld", &max);
+
+    if (n > 0 && n < 100 && max > 0 && max < 30) 
+    {
+        for (int i = 1; i <= n; i++) 
+        {
+            printf ("%d: ", i);
+
+            struct racional r1 = sorteia_r (-max, max);
+            struct racional r2 = sorteia_r (-max, max);
+
+            imprime_r (r1);
+            printf (" ");
+            imprime_r (r2);
+            printf (" ");
+            
+            if (valido_r (r1) == 0 || valido_r (r2) == 0)
+            {
+                printf ("NUMERO INVALIDO\n");
+                return 1;
+            }
+
+            struct racional result_soma = soma_r (r1, r2);
+            struct racional result_sub = subtrai_r (r1, r2);
+            struct racional result_mult = multiplica_r (r1, r2);
+            struct racional result_div = divide_r (r1, r2);
+
+
+            if (valido_r (result_div) == 0)
+            {
+                printf ("DIVISAO INVALIDA");
+                return 1;
+            }
+
+            imprime_r (result_soma);
+            printf (" ");
+            imprime_r (result_sub);
+            printf (" ");
+            imprime_r (result_mult);
+            printf (" ");
+            imprime_r (result_div);
+            printf ("\n");
+        }
+    }
+    return (0);
+}
--- tp3/racional.c
+++ tp3/racional.c
@@ -1,21 +1,11 @@
-/* 
- * Tipos Abstratos de Dados - TADs
- * Arquivo de implementação para TAD racional.
- * Feito em 20/09/2024 para a disciplina CI1001 - Programação 1.
- * Atualizado em 09/09/2025 para a disciplina CI1001 - Programação 1.
- *
- * Este arquivo deve conter as implementações das funções cujos protótipos
- * foram definidos em racional.h. Neste arquivo também podem ser definidas
- * funções auxiliares para facilitar a implementação daquelas funções.
-*/
-
 /* coloque aqui seus includes (primeiro os <...>, depois os "...") */
 #include <stdio.h>
+#include <stdlib.h>
+#include "racional.h"
 
-/* aqui vem a struct racional propriamente dita, nao modifique! */
 struct racional {
-  long num;          /* numerador   */
-  long den;          /* denominador */
+  long num;          
+  long den;          
 };
 
 /*
@@ -28,14 +18,29 @@ struct racional {
 /* calcula o mdc pelo metodo de Euclides */
 long mdc (long a, long b)
 {
-  /* implemente aqui */
+  if (a < 0) 
+  a = -a;
+
+  if (b < 0)
+  b = -b;
+
+  long resto;
+    while (b != 0) 
+    {
+      resto = a % b;
+      a = b;
+      b = resto;
+    }
+
+  return a;
 }
 
 /* Minimo Multiplo Comum entre a e b */
 /* mmc = (a * b) / mdc (a, b)        */
 long mmc (long a, long b)
 {
-  /* implemente aqui */
+  if (a == 0 || b == 0) return 0;
+  return (a * b) / mdc (a, b);
 }
 
 /* Simplifica o número racional indicado no parâmetro.
@@ -43,9 +48,91 @@ long mmc (long a, long b)
  * Retorna 1 em sucesso e 0 se r for inválido ou o ponteiro for nulo.
  * Se ambos numerador e denominador forem negativos, o resultado é positivo.
  * Se o denominador for negativo, o sinal deve migrar para o numerador. */
+
 int simplifica_r (struct racional *r)
 {
-  /* implemente aqui */
+  if (r->den == 0)
+  return r;
+  
+  long divisor = mdc (r->num, r->den);
+  r->num = r->num / divisor;
+  r->den = r->den / divisor;
+
+  if (r->den < 0)
+  {
+    r->num = -r->num;
+    r->den = -r->den;
+  }
+
+  return 1;
 }
 
 /* implemente as demais funções de racional.h aqui */
+
+/* estrutura "opaca" que representa um número racional */
+/* serve para abstrair totalmente o tipo struct racional para o usuario */
+/* a struct de fato eh implementada no arquivo racional.c */
+struct racional;
+
+/* Retorna o numerador do racional r */
+long numerador_r (struct racional *r)
+{
+  return r->num;
+}
+
+/* Retorna o denominador do racional r */
+long denominador_r (struct racional *r)
+{
+  return r->den;
+}
+
+/* Cria um número racional com o numerador e denominador indicados
+ * e retorna um ponteiro que aponta para ele.
+ * A memória para o número racional deve ser alocada dinamicamente
+ * por esta função. Retorna NULL se não conseguiu alocar a memória. */
+struct racional *cria_r (long numerador, long denominador);
+
+/* Libera a memória alocada para o racional apontado por r */
+void destroi_r (struct racional **r);
+
+/* Retorna 1 se o racional r for válido ou 0 se for inválido. Um racional
+ * é inválido se o denominador for zero ou se ele não tiver sido alocado. */
+int valido_r (struct racional *r);
+
+/* Imprime um racional r, respeitando estas regras:
+   - o racional deve estar na forma simplificada;
+   - não use espacos em branco e não mude de linha;
+   - o formato de saída deve ser "num/den", a menos dos casos abaixo;
+     - se o ponteiro for nulo, imprime a mensagem "NULL";
+     - se o racional for inválido, imprime a mensagem "NaN" (Not a Number);
+     - se o numerador for 0, imprime somente "0";
+     - se o denominador for 1, imprime somente o numerador;
+     - se o numerador e denominador forem iguais, imprime somente "1";
+     - se o racional for negativo, o sinal é impresso antes do número;
+     - se numerador e denominador forem negativos, o racional é positivo. */
+void imprime_r (struct racional *r);
+
+/* Compara dois números racionais r1 e r2.
+ * Retorna -2 se r1 ou r2 for inválido ou se o respectivo ponteiro for nulo.
+ * Retorna -1 se r1 < r2; 0 se r1 = r2; 1 se r1 > r2.
+ * Atenção: faça a comparação normalizando os denominadores pelo MMC.
+ * Fazer a comparação baseado na divisão do numerador pelo denominador
+ * pode gerar erro de arredondamento e falsear o resultado. */
+int compara_r (struct racional *r1, struct racional *r2);
+
+/* Coloca em *r3 a soma simplificada dos racionais *r1 e *r2.
+ * Retorna 1 em sucesso e 0 se r1 ou r2 for inválido ou um ponteiro for nulo. */
+int soma_r (struct racional *r1, struct racional *r2, struct racional *r3);
+
+/* Coloca em *r3 a diferença simplificada dos racionais *r1 e *r2.
+ * Retorna 1 em sucesso e 0 se r1 ou r2 for inválido ou um ponteiro for nulo. */
+int subtrai_r (struct racional *r1, struct racional *r2, struct racional *r3);
+
+/* Coloca em *r3 o produto simplificado dos racionais *r1 e *r2.
+ * Retorna 1 em sucesso e 0 se r1 ou r2 for inválido ou um ponteiro for nulo. */
+int multiplica_r (struct racional *r1, struct racional *r2, struct racional *r3);
+
+/* Coloca em *r3 a divisão simplificada do racional *r1 por *r2.
+ * Retorna 1 em sucesso e 0 se r1 ou r2 for inválido ou um ponteiro for nulo. */
+int divide_r (struct racional *r1, struct racional *r2, struct racional *r3);
+
